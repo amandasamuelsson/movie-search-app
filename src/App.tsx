69e05fc7ebb04cf37a7d7ca9805eb3d6237778b6
msmalinosterberg <msmalinosterberg@gmail.com>
2021-02-25 23:18:25 +0100

    basic header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { CSSProperties } from 'react';
 import './App.css';
+import { Layout, Menu } from 'antd';
+
 
 function App() {
+  const { Header, Footer, Content,  } = Layout;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <Layout>
+      <Header style={headerStyle}> 
+      
+        <Menu style ={menuItemStyle}>
+        <h1 style ={titleStyle}>MovieApp Name </h1> 
+           <Menu.Item >Home</Menu.Item>
+           <Menu.Item>Watchlist</Menu.Item>
+           <Menu.Item>Seen</Menu.Item>
+        </Menu>
+      </Header>
+      
+      
+      <Content></Content>
+      <Footer></Footer>
+    </Layout>
+   
   );
 }
 
+const headerStyle: CSSProperties ={
+  background: 'black',
+  height: '4.5rem',
+  position: 'absolute',
+  width: '100%', 
+  padding: '0',
+  margin: '0',
+  color: 'white'
+  
+}
+
+
+const menuItemStyle: CSSProperties = {
+  display: 'flex',
+  position: 'relative', 
+  color: 'white', 
+  justifyContent: 'space-evenly', 
+  listStyleType: 'none',
+  alignItems: 'center',
+  marginTop: '0'
+  
+  
+}
+
+const titleStyle: CSSProperties = {
+  justifyItems:'flex-start',
+  fontStyle: 'italic'
+}
 export default App;
