01e2f74ddeef18b2e40f59088b3c817b301540ec
msmalinosterberg <msmalinosterberg@gmail.com>
2021-02-26 13:11:13 +0100

    navbar layout done with logo and links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,63 +1,62 @@
 import { Menu } from 'antd';
-import React, { CSSProperties } from "react";
 import { Layout } from 'antd';
+import { CSSProperties } from 'react';
+import logo from '../assets/logo.png';
+
 
 
 function Navbar () {
-  const { Header, Footer, Content,  } = Layout;
+  const { Header } = Layout;
 
 return ( 
- <Layout>
- <Header style={headerStyle}> 
  
-   <Menu style ={menuItemStyle}>
-   <h1 style ={titleStyle}>MovieApp Name </h1> 
-      <Menu.Item >Home</Menu.Item>
-      <Menu.Item>Watchlist</Menu.Item>
-      <Menu.Item>Seen</Menu.Item>
-   </Menu>
- </Header>
- 
- 
- <Content></Content>
- <Footer></Footer>
+<Layout style={layoutStyle}>
+  <Header style={{background: 'black', textDecoration:'none'}}>
+    <img src= {logo} alt="logo" style={logoStyle} />
+    <Menu mode="horizontal" style= {menuStyle}>
+      <Menu.Item key="1">Home</Menu.Item>
+      <Menu.Item key="2">Watchlist</Menu.Item>
+      <Menu.Item key="3">Seen</Menu.Item>
+    </Menu>
+  </Header>
+  
 </Layout>
 
 );
+
 }
 
 
 
-const headerStyle: CSSProperties ={
-    background: 'black',
-    height: '4.5rem',
-    position: 'absolute',
-    width: '100%', 
-    padding: '0',
-    margin: '0',
-    color: 'white'
-    
-  }
+
+const layoutStyle: CSSProperties = {
+  float: 'left', 
+  width: '100%', 
+  background: 'black',
+  height: '6rem',
+  textDecoration:'none',
   
-  
-  const menuItemStyle: CSSProperties = {
-    display: 'flex',
-    position: 'relative', 
-    color: 'white', 
-    justifyContent: 'space-evenly', 
-    listStyleType: 'none',
-    alignItems: 'center',
-    marginTop: '0'
-    
-    
-  }
-  
-  const titleStyle: CSSProperties = {
-    justifyItems:'flex-start',
-    fontStyle: 'italic'
-  }
+}
+ 
 
+const menuStyle: CSSProperties = {
+  float: 'right',
+  background: 'black', 
+  color: 'white', 
+  display: 'flex', 
+  justifyContent: 'space-evenly',
+  alignItems: 'center', 
+  textDecorationLine : 'none'
+}
 
+const logoStyle: CSSProperties = {
+  height: '4rem',
+  margin: '1rem',
+  width: '12.5rem'
 
-
+}
 export default Navbar; 
+ 
+ 
+
+ 
