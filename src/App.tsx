48b3630379d43a91260db30c74ce3c08c0d86ea9
Moa Stenqvist <moa@stonetwig.se>
2021-02-26 13:16:21 +0100

    Add watchlistView, table + title.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import MovieDetails from './components/movieDetails/MovieDetails';
 import SearchView from './components/searchPage/SearchView';
+import WatchlistView from './components/watchlistPage/WatchlistView';
 
 function App() {
   return (
     //<MovieDetails/>
     //<TableSearchResults/>
     //<TitleSearch/>
-    <SearchView/>
+    //<SearchView/>
+    <WatchlistView/>
   );
 }
 
