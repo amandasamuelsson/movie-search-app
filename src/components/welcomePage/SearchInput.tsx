4bb8bc125dfcf23b79a4c9bc6217265e981b3140
Amanda <amanda.e.samuelsson@hotmail.com>
2021-02-26 14:22:44 +0100

    Added search inputs

--- src/components/welcomePage/SearchInput.tsx
+++ src/components/welcomePage/SearchInput.tsx
@@ -1,1 +1,27 @@
-export {}
+import React, { CSSProperties } from 'react';
+import { Input, Space } from 'antd';
+
+const { Search } = Input;
+export default function SearchInput() {
+
+
+const onSearch = (value: String) => console.log(value);
+
+return (
+    <div>
+        <Space direction="vertical">
+            <Search style={searchInput} placeholder="input search text" onSearch={onSearch} enterButton />
+        </Space>
+  </div>
+);
+}
+
+const searchInput: CSSProperties = {
+    width: '100%',
+    height: '100vh',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '2rem'
+
+}
+
