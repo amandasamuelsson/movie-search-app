1ccf4e5baf35dc9500ed3cf32e63934e3da4a3ea
msmalinosterberg <msmalinosterberg@gmail.com>
2021-02-26 14:35:00 +0100

    added backgroundimage

--- src/components/welcomePage/Titel.tsx
+++ src/components/welcomePage/Titel.tsx
@@ -1,1 +1,32 @@
-export {}
+import Layout from 'antd/lib/layout/layout';
+import React, { CSSProperties } from 'react'; 
+import background from '../../assets/background.png';
+
+
+
+
+function WelcomeTitle() {
+    return (
+        <Layout>
+            <img src={background} alt="backgroundPicture" style={backgroundImageStyle} />
+
+            <div style={welcomeTextStyle}> 
+                <h1>Welcome to your favourite movie search app!</h1>
+                <h2>Search for movies, see the details and rating, add them to your watchlist</h2>
+            </div>
+        </Layout>
+
+    )
+}
+
+const backgroundImageStyle: CSSProperties={
+    backgroundSize: 'cover', 
+}
+
+const welcomeTextStyle: CSSProperties ={
+    color: 'white', 
+
+    
+}
+
+export default WelcomeTitle; 
