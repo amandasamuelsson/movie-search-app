2b6b15c8b0f4e9afa0f2412670b906a6465754e8
Moa Stenqvist <moa@stonetwig.se>
2021-02-26 10:25:07 +0100

    Add basic table with action icons.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MovieDetails from './components/movieDetails/MovieDetails';
+import TableSearchResults from './components/searchPage/TableSearchresults';
 
 function App() {
   return (
-    <MovieDetails/>
+    // <MovieDetails/>
+    <TableSearchResults/>
   );
 }
 
--- src/components/searchPage/TableSearchresults.tsx
+++ src/components/searchPage/TableSearchresults.tsx
@@ -1,1 +1,66 @@
-export {}
+import React, { CSSProperties } from 'react';
+import { Table, Space, Row, Col } from 'antd';
+import { PlusCircleFilled, EyeFilled, DeleteFilled } from '@ant-design/icons';
+
+
+const columns = [
+  {
+    title: 'Titel',
+    dataIndex: 'title',
+    key: 'title',
+    render: (text: any) => <a>{text}</a>,
+  },
+  {
+    title: 'IMDB Score',
+    dataIndex: 'imdbScore',
+    key: 'imdbScore',
+  },
+  {
+    title: 'Actions',
+    key: 'action',
+    render: (text: any, record: any) => (
+      <Space size="middle">
+        <a><PlusCircleFilled style={iconStyle}/></a>
+        <a><EyeFilled style={iconStyle}/></a>
+        <a><DeleteFilled style={iconStyle}/></a>
+      </Space>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    title: 'Movie Title1',
+    imdbScore: '8.9',
+  },
+  {
+    key: '2',
+    title: 'Movie Title2',
+    imdbScore: '7.4',
+  },
+  {
+    key: '3',
+    title: 'Movie Title3',
+    imdbScore: '8.4',
+  },
+];
+
+export default function TableSearchResults() {
+    return (
+        <Row style={tableContainer}>
+            <Col lg={{span: 24}}>
+                <Table columns={columns} dataSource={data} />
+            </Col>
+        </Row>
+    );
+}
+
+const tableContainer: CSSProperties = {
+    margin: '3rem',
+}
+
+const iconStyle: CSSProperties = {
+    color: 'grey',
+    fontSize: '1.1rem',
+}
