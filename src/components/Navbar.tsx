ebd0218d68e01f9d383b257e6c643f2b78f93b56
msmalinosterberg <msmalinosterberg@gmail.com>
2021-02-26 09:59:29 +0100

    navbar file working

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,1 +1,63 @@
-export {}
+import { Menu } from 'antd';
+import React, { CSSProperties } from "react";
+import { Layout } from 'antd';
+
+
+function Navbar () {
+  const { Header, Footer, Content,  } = Layout;
+
+return ( 
+ <Layout>
+ <Header style={headerStyle}> 
+ 
+   <Menu style ={menuItemStyle}>
+   <h1 style ={titleStyle}>MovieApp Name </h1> 
+      <Menu.Item >Home</Menu.Item>
+      <Menu.Item>Watchlist</Menu.Item>
+      <Menu.Item>Seen</Menu.Item>
+   </Menu>
+ </Header>
+ 
+ 
+ <Content></Content>
+ <Footer></Footer>
+</Layout>
+
+);
+}
+
+
+
+const headerStyle: CSSProperties ={
+    background: 'black',
+    height: '4.5rem',
+    position: 'absolute',
+    width: '100%', 
+    padding: '0',
+    margin: '0',
+    color: 'white'
+    
+  }
+  
+  
+  const menuItemStyle: CSSProperties = {
+    display: 'flex',
+    position: 'relative', 
+    color: 'white', 
+    justifyContent: 'space-evenly', 
+    listStyleType: 'none',
+    alignItems: 'center',
+    marginTop: '0'
+    
+    
+  }
+  
+  const titleStyle: CSSProperties = {
+    justifyItems:'flex-start',
+    fontStyle: 'italic'
+  }
+
+
+
+
+export default Navbar; 
